ea42a34767641e118ff6f33d7d09b509de4b95e4
mdronytalukder42 <mdronytalukder42@gmail.com>
2025-11-13 18:50:53 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello, Siraj Hosen Rony!</h1>
+      <p>Your Vercel build is working now 🎉</p>
+    </div>
+  );
+}
+
+export default App;
